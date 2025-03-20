0bfc2b36a20db92bf74c9128f46cdf1369691c36
Selvam-T <thiagarajan.selvam@gmail.com>
2025-03-20 14:03:45 +0800

    wp-config.php created using entrypoint.sh in runtime

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wp config create \
+    --dbname="${WORDPRESS_DB_NAME}" \
+    --dbhost="${WORDPRESS_DB_HOST}" \
+    --dbuser="${WORDPRESS_DB_USER}" \
+    --dbpass="${WORDPRESS_DB_PASSWORD}" \
+    --path=/var/www/html \
+    --allow-root \
+    --skip-check
+wp config set WP_HOME "https://${DOMAIN_NAME}:${PORT}" --type=constant --path=/var/www/html --allow-root
+wp config set WP_SITEURL "https://${DOMAIN_NAME}:${PORT}/wp-admin" --type=constant --path=/var/www/html --allow-root
+exec "$@"
